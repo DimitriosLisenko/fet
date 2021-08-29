eb37465c9da676a06d70da7664443342e2529f50
Dima <dimitrioslisenko@gmail.com>
2021-08-29 20:59:49 +0100

    Move note validations to a separate module

--- lib/fet/note.rb
+++ lib/fet/note.rb
@@ -1,8 +1,12 @@
 # frozen_string_literal: true
 
+require_relative "note_validations"
+
 module Fet
   # Class responsible for parsing and validating musical notes from a string, e.g. "Eb"
   class Note
+    include NoteValidations
+
     ACCIDENTAL_TO_SEMITONES_MAP = {
       "b" => -1,
       "#" => 1,
@@ -110,56 +114,6 @@ module Fet
     end
 
     private
-
-    def validate_full_note!
-      raise InvalidNote.new(full_note) unless full_note.is_a?(String)
-    end
-
-    def validate_natural_note!
-      raise InvalidNote.new(full_note) unless natural_note.is_a?(String)
-      raise InvalidNote.new(full_note) unless MusicTheory::CIRCLE_OF_FIFTHS_WITHOUT_ACCIDENTALS.include?(natural_note)
-    end
-
-    def validate_accidental!
-      raise InvalidNote.new(full_note) unless valid_accidental_type?
-      return if accidental.empty?
-
-      raise InvalidNote.new(full_note) unless valid_accidental_logic?
-    end
-
-    def valid_accidental_type?
-      return accidental.is_a?(String)
-    end
-
-    def valid_accidental_logic?
-      return false unless valid_accidental_characters?
-      return false if can_be_flat_accidental? && !valid_flat_accidental?
-      return false if can_be_sharp_accidental? && !valid_sharp_accidental?
-
-      return true
-    end
-
-    def valid_accidental_characters?
-      return accidental.chars.uniq.all? { |char| ["b", "#", "x"].include?(char) }
-    end
-
-    def can_be_flat_accidental?
-      return accidental.chars.uniq.include?("b")
-    end
-
-    def valid_flat_accidental?
-      return accidental.chars.uniq.all? { |char| ["b"].include?(char) } # there can only be "b" characters
-    end
-
-    def can_be_sharp_accidental?
-      return accidental.chars.uniq.include?("#") || accidental.chars.uniq.include?("x")
-    end
-
-    def valid_sharp_accidental?
-      return accidental.chars.uniq.all? { |char| ["#", "x"].include?(char) } && # only allow "#" and "x" characters
-             [0, 1].include?(accidental.chars.select { |char| char == "#" }.size) && # there can be at most one "#" character
-             accidental.chars.sort.join == accidental # the "#" character must come before any "x" characters
-    end
 
     def next_natural_note
       index = Fet::MusicTheory::ORDERED_NATURAL_NOTES.index(natural_note)
