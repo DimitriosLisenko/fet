6e46aff5e9b7cb7fe059e0b509ad7d92890d811a
Dima <dimitrioslisenko@gmail.com>
2021-08-30 02:15:51 +0100

    Satisfy minitest

---
+++ test/fet/chord_progression_test.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+module Fet
+  class ChordProgressionTest < Minitest::Test
+    def test_major_chord_progression
+      template = Fet::ChordProgression::DEFAULT_TEMPLATES[Fet::ChordProgression::TEMPLATE_MAJOR]
+      major = Fet::ChordProgression.new(offset: 0, template_type: Fet::ChordProgression::TEMPLATE_MAJOR)
+      assert_equal(template, major.with_offset)
+
+      major = Fet::ChordProgression.new(offset: 10, template_type: Fet::ChordProgression::TEMPLATE_MAJOR)
+      assert_equal(template.flatten.map { |x| x + 10 }, major.with_offset.flatten)
+    end
+
+    def test_minor_chord_progression
+      template = Fet::ChordProgression::DEFAULT_TEMPLATES[Fet::ChordProgression::TEMPLATE_MINOR]
+      minor = Fet::ChordProgression.new(offset: 0, template_type: Fet::ChordProgression::TEMPLATE_MINOR)
+      assert_equal(template, minor.with_offset)
+
+      minor = Fet::ChordProgression.new(offset: 10, template_type: Fet::ChordProgression::TEMPLATE_MINOR)
+      assert_equal(template.flatten.map { |x| x + 10 }, minor.with_offset.flatten)
+    end
+
+    def test_invalid_chord_progression
+      assert_raises(Fet::InvalidChordProgression) do
+        Fet::ChordProgression.new(offset: "offset", template_type: Fet::ChordProgression::TEMPLATE_MINOR)
+      end
+
+      assert_raises(Fet::InvalidChordProgression) do
+        Fet::ChordProgression.new(offset: 0, template_type: "hello")
+      end
+
+      assert_raises(Fet::InvalidChordProgression) do
+        Fet::ChordProgression.new(offset: 0, template: ["hello", "world"])
+      end
+    end
+  end
+end
