3364621acb5afb08b796cc250afde914a94ffc92
Dima <dimitrioslisenko@gmail.com>
2021-09-01 02:09:10 +0100

    Fail tests unless 100% test coverage is met

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,6 +5,7 @@ SimpleCov.start do
   enable_coverage :branch
 end
 
+SimpleCov.minimum_coverage(line: 100, branch: 100)
 unless ENV["CODECOV_TOKEN"].nil?
   require "codecov"
   SimpleCov.formatter = SimpleCov::Formatter::Codecov
