5a8b916d8c3c8f896a21936c73fb3a811c4c1d62
Dima <dimitrioslisenko@gmail.com>
2021-09-02 22:32:35 +0100

    Add color scheme to the project

---
+++ lib/fet/ui/color_scheme.rb
@@ -0,0 +1,11 @@
+module Fet
+  module Ui
+    class ColorScheme
+      BLACK = "#151727".deep_freeze
+      GREY = "#393D56".deep_freeze
+      WHITE = "#FAF9F0".deep_freeze
+      RED = "#931621".deep_freeze
+      GREEN = "#2F754E".deep_freeze
+    end
+  end
+end
