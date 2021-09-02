96154041e42b66b2c9bd60c7ac65c9a186f08867
Dima <dimitrioslisenko@gmail.com>
2021-09-02 23:02:13 +0100

    Add score object

---
+++ lib/fet/score.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+module Fet
+  # Holds the correct/incorrect answers to questions for this session
+  class Score
+    def initialize
+      self.score = initialize_score
+    end
+
+    def answer_correctly(degree)
+      key = keys.detect { |k| k.include?(degree) }
+      score[key][:correct] += 1
+    end
+
+    def answer_incorrectly(degree)
+      key = keys.detect { |k| k.include?(degree) }
+      score[key][:incorrect] += 1
+    end
+
+    def answered_correctly(degree = nil)
+      return score.reduce(0) do |result, (degree_names, score_hash)|
+        degree.nil? || degree_names.include?(degree) ? result + score_hash[:correct] : result
+      end
+    end
+
+    def answered_incorrectly(degree = nil)
+      return score.reduce(0) do |result, (degree_names, score_hash)|
+        degree.nil? || degree_names.include?(degree) ? result + score_hash[:incorrect] : result
+      end
+    end
+
+    def questions_asked(degree = nil)
+      return answered_correctly(degree) + answered_incorrectly(degree)
+    end
+
+    private
+
+    attr_accessor :score
+
+    def initialize_score
+      Fet::Degree::DEGREE_NAMES.transform_values { |_| { correct: 0, incorrect: 0 } }
+    end
+  end
+end
