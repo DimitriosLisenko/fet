9d26412567b7b6daacfe0575c218654e8ca3249b
Dima <dimitrioslisenko@gmail.com>
2021-09-02 23:16:28 +0100

    Also play using keys

--- lib/fet/ui/note_box.rb
+++ lib/fet/ui/note_box.rb
@@ -23,12 +23,51 @@ module Fet
       end
 
       def handle_event(event)
+        handle_click_event(event)
+        handle_key_event(event)
+      end
+
+      def handle_click_event(event)
         return unless event.is_a?(Ruby2D::Window::MouseEvent)
         return unless event.type == :down
         return unless event.button == :left
         return unless square.contains?(event.x, event.y)
 
         music.play
+      end
+
+      def handle_key_event(event)
+        return unless event.is_a?(Ruby2D::Window::KeyEvent)
+        return unless event.type == :down
+
+        play = case event.key
+               when "a"
+                 text == "1"
+               when "s"
+                 text == "2"
+               when "d"
+                 text == "3"
+               when "f"
+                 text == "4"
+               when "g"
+                 text == "5"
+               when "h"
+                 text == "6"
+               when "j"
+                 text == "7"
+               when "w"
+                 text == "b2"
+               when "e"
+                 text == "b3"
+               when "t"
+                 text == "b5"
+               when "y"
+                 text == "b6"
+               when "u"
+                 text == "b7"
+               end
+
+        music.play if play
       end
     end
   end
