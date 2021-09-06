1f03ca2a1a001edba0c8938651a399fa49c43d87
Dima <dimitrioslisenko@gmail.com>
2021-09-06 19:21:51 +0100

    Split timer update into separate functions

--- test/fet/ui/game_update_loop_tester.rb
+++ test/fet/ui/game_update_loop_tester.rb
@@ -10,17 +10,24 @@ module Fet
         Timecop.freeze(now)
         game = Fet::Ui::Game.new(tempo: 200, degrees: 1, key_type: "major", next_on_correct: false)
 
-        # NOTE: Timecop does not yet support freezing Process.clock_gettime
-        timer = game.timer
-        def timer.current_time
-          return Time.now.to_i
+        stub_game_timer_current_time do
+          game_instance_test(game) do
+            Timecop.freeze(now + (10.0 / 24 / 60 / 60))
+            game.handle_update_loop
+            assert_equal(10, game.timer.seconds_elapsed)
+          end
         end
+      end
 
-        game_instance_test(game) do
-          Timecop.freeze(now + (10.0 / 24 / 60 / 60))
-          game.handle_update_loop
-          assert_equal(10, game.timer.seconds_elapsed)
-        end
+      private
+
+      # NOTE: Timecop does not yet support freezing Process.clock_gettime, so do this in the meantime
+      def stub_game_timer_current_time
+        Fet::Ui::Timer.stub_any_instance(:current_time, method(:unix_time)) { yield }
+      end
+
+      def unix_time
+        Time.now.to_i
       end
     end
   end
