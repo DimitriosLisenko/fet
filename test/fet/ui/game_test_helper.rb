10a29447f2f78efdb2f733c58ef4f0f917f1fb7f
Dima <dimitrioslisenko@gmail.com>
2021-09-06 01:15:54 +0100

    Completely mock Ruby2D::Text and Ruby2D::Music because they load some external UI processes

--- test/fet/ui/game_test_helper.rb
+++ test/fet/ui/game_test_helper.rb
@@ -88,15 +88,15 @@ module Fet
       end
 
       def with_stubs
-        stub_window_show do
-          stub_window_close do
-            stub_music_instance_play do
-              stub_score_writing do
-                yield
-              end
-            end
-          end
-        end
+        stub_ruby2d_objects { stub_our_objects { yield } }
+      end
+
+      def stub_our_objects
+        stub_score_writing { yield }
+      end
+
+      def stub_ruby2d_objects
+        stub_window_show { stub_window_close { stub_music_instance_play { stub_music_initialize { stub_text_initialize { yield } } } } }
       end
 
       def stub_window_show
@@ -109,6 +109,14 @@ module Fet
 
       def stub_music_instance_play
         Ruby2D::Music.stub_any_instance(:play, nil) { yield }
+      end
+
+      def stub_text_initialize
+        Ruby2D::Text.stub(:new, FakeText.method(:new)) { yield }
+      end
+
+      def stub_music_initialize
+        Ruby2D::Music.stub(:new, FakeMusic.method(:new)) { yield }
       end
 
       def stub_score_writing
@@ -138,6 +146,37 @@ module Fet
                   end
         result << degree.degree_value.to_s
         return result.compact
+      end
+    end
+
+    # NOTE: this is required because it seems that at the moment even initializing the Ruby2D::Text object
+    # starts up a Ruby process that I guess is supposed to be the UI, and makes GitHub actions fail with
+    # Error: (SDL_Init) No available video device
+    # NOTE: if this resolves the issue of the Ruby process starting, then maybe we can run the game in a thread for tests
+    class FakeText
+      attr_accessor :text, :color, :x, :y, :size, :font
+
+      def initialize(text, **args)
+        self.text = text
+        self.color = args[:color]
+        self.x = args[:x]
+        self.y = args[:y]
+        self.size = args[:size]
+        self.font = args[:font]
+      end
+    end
+
+    # NOTE: also load Ruby process as Ruby2D::Text
+    class FakeMusic
+      attr_accessor :path, :loop, :number_of_times_played
+
+      def initialize(path)
+        self.path = path
+        self.number_of_times_played = 0
+      end
+
+      def play
+        self.number_of_times_played += 1
       end
     end
   end
