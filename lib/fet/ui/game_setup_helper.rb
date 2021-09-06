de416f0d63652c053d98f268c89eb699a0bb7ee2
Dima <dimitrioslisenko@gmail.com>
2021-09-06 19:36:58 +0100

    Don't test coverage by sending Ruby2D events

--- lib/fet/ui/game_setup_helper.rb
+++ lib/fet/ui/game_setup_helper.rb
@@ -23,6 +23,8 @@ module Fet
         Ruby2D::Window.set(background: Fet::Ui::ColorScheme::BLACK)
       end
 
+      # NOTE: don't test coverage for these methods because this is more of a test of the Ruby2D library
+      # :nocov:
       def setup_window_event_loop
         Ruby2D::Window.on(:key_down) do |event|
           handle_event_loop(event)
@@ -32,12 +34,16 @@ module Fet
           handle_event_loop(event)
         end
       end
+      # :nocov:
 
+      # NOTE: don't test coverage for these methods because this is more of a test of the Ruby2D library
+      # :nocov:
       def setup_window_update_loop
         Ruby2D::Window.update do
           handle_update_loop
         end
       end
+      # :nocov:
     end
   end
 end
