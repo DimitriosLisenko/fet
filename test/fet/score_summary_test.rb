dbad53cbde658648fcfe4b10b66be1abf14e3263
Dima <dimitrioslisenko@gmail.com>
2021-09-14 00:31:33 +0100

    Start testing ScoreSummary

---
+++ test/fet/score_summary_test.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+module Fet
+  class ScoreSummaryTest < Minitest::Test
+    def test_summary
+      stub_puts do
+        stub_scores_filename(scores_fixture) do
+          Fet::ScoreSummary.new.summary
+        end
+      end
+    end
+
+    private
+
+    def stub_puts
+      Fet::ScoreSummary.stub_any_instance(:puts, puts_stub) { yield }
+    end
+
+    def puts_stub
+      return lambda { |value| return value }
+    end
+
+    def stub_scores_filename(filename)
+      Fet::ScoreSummary.stub(:scores_filename, filename) { yield }
+    end
+
+    def scores_fixture
+      return File.join(Fet.root, "test", "fet", "fixtures", "scores")
+    end
+
+    def nonexistent_scores_filename
+      return File.join(Fet.root, "test", "fet", "fixtures", "chores")
+    end
+  end
+end
